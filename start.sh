9e48f3622282a8a9ec1de4b3211341049f11e62a
Cyber Phantom <inam123451@gmail.com>
2024-10-07 10:35:22 +0530

    Update

--- start.sh
+++ start.sh
@@ -5,4 +5,4 @@ java -Xms2G -Xmx4G \
      -XX:ParallelGCThreads=2 \
      -XX:+DisableExplicitGC \
      -XX:+UnlockExperimentalVMOptions \
-     -jar quilt-server-launch.jar
+     -jar fabric-server-mc.1.21-loader.0.16.5-launcher.1.0.1.jar
