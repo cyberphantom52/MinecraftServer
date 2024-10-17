e9871f0bcc9aba0830c789d7d90cc771875cd329
Cyber Phantom <inam123451@gmail.com>
2024-10-17 20:21:38 +0530

    Update

--- start.sh
+++ start.sh
@@ -1,8 +1,23 @@
 #!/bin/bash
-
-java -Xms2G -Xmx4G \
+   
+java -Xms4000M -Xmx6500M \
      -XX:+UseG1GC \
-     -XX:ParallelGCThreads=2 \
-     -XX:+DisableExplicitGC \
      -XX:+UnlockExperimentalVMOptions \
-     -jar fabric-server-mc.1.21-loader.0.16.5-launcher.1.0.1.jar
+     -XX:G1NewSizePercent=30 \
+     -XX:G1MaxNewSizePercent=40 \
+     -XX:G1HeapWastePercent=5 \
+     -XX:G1ReservePercent=20 \
+     -XX:G1HeapRegionSize=8M \
+     -XX:G1MixedGCCountTarget=4 \
+     -XX:G1MixedGCLiveThresholdPercent=90 \
+     -XX:G1RSetUpdatingPauseTimePercent=5 \
+     -XX:MaxGCPauseMillis=200 \
+     -XX:InitiatingHeapOccupancyPercent=20 \
+     -XX:SurvivorRatio=32 \
+     -XX:+PerfDisableSharedMem \
+     -XX:MaxTenuringThreshold=1 \
+     -XX:+ParallelRefProcEnabled \
+     -XX:+AlwaysPreTouch -XX:+DisableExplicitGC \
+     -XX:+UseStringDeduplication \
+     -jar fabric-server-mc.1.21-loader.0.16.5-launcher.1.0.1.jar nogui
+
