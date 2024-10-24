e6b96c7a71628fc2f49f07e15ae2c6a6fe3205de
Cyber Phantom <inam123451@gmail.com>
2024-10-24 17:06:13 +0530

    Update

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-   
+
 java -Xms4000M -Xmx6500M \
      -XX:+UseG1GC \
      -XX:+UnlockExperimentalVMOptions \
@@ -20,4 +20,3 @@ java -Xms4000M -Xmx6500M \
      -XX:+AlwaysPreTouch -XX:+DisableExplicitGC \
      -XX:+UseStringDeduplication \
      -jar fabric-server-mc.1.21-loader.0.16.5-launcher.1.0.1.jar nogui
-
